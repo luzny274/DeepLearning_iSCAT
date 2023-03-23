bec9c7628c3caa0950c377549bc9f08eb77b045e
Vítězslav Lužný <luzny@ufe.cz>
2023-03-23 17:08:23 +0100

    update

--- DL_regression/training.py
+++ DL_regression/training.py
@@ -1,5 +1,8 @@
 
-import DL_SampleGeneration
+import os
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")  # Report only TF errors by default
+
+import DL_Dataset
 
 import glob
 import time
@@ -10,36 +13,28 @@ from matplotlib.patches import Rectangle
 
 from ipywidgets import Layout, interact, IntSlider, FloatSlider
 
-gen = DL_SampleGeneration.SampleGenerator()
-
-exD = 5000
-devD = 2500
-
-exPT_cnt = 100
-devPT_cnt = 99
-
-sample_cnt = 100000
-
-exIntensity = 1.0
-devIntensity = 0.7
-
-step_cnt = gen.step_cnt
-res = gen.cam_fov_px
-
-import os
-os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")  # Report only TF errors by default
 import tensorflow as tf
+
+
+
+train_gen = DL_Dataset.iSCAT_DataGenerator(batch_size=1024, epoch_size=65536, res=32, frames=32, thread_count=18,
+                PSF_path="../PSF_subpx_fl32.npy", exD=5000, devD=4000, exPT_cnt=1000, devPT_cnt=999, exIntensity=1.0, devIntensity=0.9, target_step=31,
+                target_mode="count_particles")
+
+test_gen = DL_Dataset.iSCAT_DataGenerator(batch_size=128, epoch_size=256, res=32, frames=32, thread_count=18,
+                PSF_path="../PSF_subpx_fl32.npy", exD=5000, devD=4000, exPT_cnt=1000, devPT_cnt=999, exIntensity=1.0, devIntensity=0.9, target_step=31,
+                target_mode="count_particles")
+
 
 seed = 42
 tf.keras.utils.set_random_seed(seed)
 
-hidden_layer = 1000
-batch_size = 32
+hidden_layer = 5000
 epochs = 10
 
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=[step_cnt, res, res]),
+    tf.keras.layers.Flatten(input_shape=[train_gen.step_cnt, train_gen.cam_fov_px, train_gen.cam_fov_px]),
     tf.keras.layers.Dense(hidden_layer, activation=tf.nn.relu),
     tf.keras.layers.Dense(1)
 ])
@@ -48,34 +43,14 @@ optimizer = tf.optimizers.Adam()
 
 model.compile(
     optimizer=optimizer,
-    loss=tf.keras.losses.MeanSquaredError(),
+    # loss=tf.keras.losses.MeanSquaredError(),
+    loss = tf.keras.losses.Huber(),
+    metrics=[tf.keras.metrics.MeanAbsoluteError(name="accuracy")],
 )
 
-timing = time.time()
-samples, particle_positions, diffusion_coefs, pt_cnts, particles_in_sight_cnt, avg_diffusions = gen.GenSamples(exD, devD, exPT_cnt, devPT_cnt, sample_cnt, exIntensity, devIntensity, gen.step_cnt-1)
-print("Sample generation took: {:.3f}s".format(time.time() - timing))
-
-samples = np.array(samples)
-particles_in_sight_cnt = np.array(particles_in_sight_cnt)
-avg_diffusions = np.array(avg_diffusions)
-
-
-class DataGenerator(tf.keras.utils.Sequence):
-    def __init__(self, x_set, y_set, batch_size):
-        self.x, self.y = x_set, y_set
-        self.batch_size = batch_size
-
-    def __len__(self):
-        return int(np.ceil(len(self.x) / float(self.batch_size)))
-
-    def __getitem__(self, idx):
-        batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
-        batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
-        return batch_x, batch_y
-
-train_gen = DataGenerator(samples, avg_diffusions, batch_size)
+model.summary()
 
 model.fit(
-    train_gen,
-    epochs=epochs
-)
+            x=train_gen, validation_data=test_gen,
+            epochs=epochs
+        )
